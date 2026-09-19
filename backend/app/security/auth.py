f01914b6d8a79926fb0c6015da17bef573b40652
PiyushKumar74110 <piyushkumar74110@gmail.com>
2026-09-19 17:38:58 +0530

    update auth.y

--- backend/app/security/auth.py
+++ backend/app/security/auth.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
 from jose import jwt
+from app.config import settings
 
-SECRET_KEY = "CHANGE_THIS_TO_A_RANDOM_SECRET"
+
+SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
 
 def create_access_token(data: dict):
